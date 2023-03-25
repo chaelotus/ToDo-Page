365dc0512dcdbbcf103d81205dba5ac1c7b14bc1
parkchaeyeon <pcy6904@naver.com>
2023-03-26 00:28:32 +0900

    feat: x버튼 추가

--- app.js
+++ app.js
@@ -3,18 +3,28 @@ const todosDisplay = document.querySelector(".todos-display__container");
 const $input = document.querySelector(".todos-input__input");
 //li 만들어서 ul에 포함시키기
 const createItem = (content) => {
-  const checkbox = document.createElement("input");
-  checkbox.type = "checkbox";
   const ul = document.createElement("ul");
   ul.classList.add("todos-display__items");
+
+  const div_list = document.createElement("div");
+  div_list.classList.add("todos-display__list");
+
+  const div_check = document.createElement("div");
+  div_check.classList.add("todos-display__check");
+
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
   const li = document.createElement("li");
   li.classList.add("todos-display__items-item");
   li.textContent = content;
+  div_check.append(checkbox, li);
+  const deleteicon = document.createElement("i");
+  deleteicon.classList.add("fa-solid");
+  deleteicon.classList.add("fa-xmark");
+
   todosDisplay.append(ul);
-  const div = document.createElement("div");
-  div.classList.add("todos-display__list");
-  ul.append(div);
-  div.append(checkbox, li);
+  ul.append(div_list);
+  div_list.append(div_check, deleteicon);
 };
 $form.onkeydown = (event) => {
   if (event.keyCode === 13) {
